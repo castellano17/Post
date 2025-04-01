d7718ea0adafaaf3de3e9a3afcc5b8fa72cbd0d5
castellano17 <castellano17@gmail.com>
2025-04-01 10:52:41 -0600

    add status friends ;

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -8,7 +8,12 @@
 
     <div class="flex justify-between">
         {{ $user->name }}
-       <x-submit-button>Confirm</x-submit-button>
+      
+       <form action="{{ route('friends.update', $user) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <x-submit-button>Confirm</x-submit-button>
+       </form>
     </div>
  
   
